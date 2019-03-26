d37849f7b72f864f07bbfdb278a2e6b85a51adb1
ArduCAM <admin@arducam.com>
2019-03-26 19:17:01 +0800

    removed driver

--- MIPI_Camera/ov7251-release/4.14.79-v7+/loaddepends.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-modinfo ./arducam-ov7251-v4l2-pi3.ko | grep depends | awk '{print $NF}' | \
- sed 's/,/ /g' | xargs sudo modprobe -a
--- MIPI_Camera/ov9281-release/4.14.71-v7+/loaddepends.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-modinfo ./arducam-ov9281-v4l2-pi3.ko | grep depends | awk '{print $NF}' | \
- sed 's/,/ /g' | xargs sudo modprobe -a
