024cd964b54a1f49a9bc0f82b994ee0daaed42f4
UCTRONICS <avahe.uctronics.com>
2019-05-08 11:44:03 +0800

    update

--- Multi_Camera_Adapter/Multi_Adapter_Board_2Channel/shell/pi_zero_cam.sh
+++ Multi_Camera_Adapter/Multi_Adapter_Board_2Channel/shell/pi_zero_cam.sh
@@ -16,7 +16,7 @@ rev=`cat /proc/cpuinfo | grep Revision | awk '{print substr($NF,length($NF)-5,6)
 #echo "Detected the board revsion: $rev"
 case $rev in
 '9000c1')
-echo "The board is PiZero W, we control the GPIO40"
+echo "The board is PiZero W."
 gpio -g write 40 1 #set the gpio40 high
 echo "Choose camera A"
 raspistill -o camera1.jpg
@@ -25,8 +25,18 @@ echo "Choose Camera B"
 raspistill -o camera2.jpg
 echo "Test OK"
 ;;
+'a02082'|'a22082'|'a020d3')
+echo "The board is Pi 3B/B+."
+./rpi3-gpiovirtbuf s 134 1 #set the gpio134 high
+echo "Choose camera A"
+raspistill -o camera1.jpg
+./rpi3-gpiovirtbuf s 134 0 #set the gpio134 low
+echo "Choose Camera B"
+raspistill -o camera2.jpg
+echo "Test OK"
+;;
 '0010'|'0012'|'0013'|'1041'|'a01041'|'a21041'|'a02042'|'900093'|'920093')
-echo "The board is PiZero, we control the GPIO32"
+echo "The board is PiZero."
 gpio -g write 32 1 #set the gpio32 high
 echo "Choose camera A"
 raspistill -o camera1.jpg
