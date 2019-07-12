5374763746771eb052e4d8482ddc59bda0942b95
Lee <admin@arducam.com>
2019-07-12 13:27:39 +0800

    Update Motorized_Focus_Camera_Snapshot.py

--- Motorized_Focus_Camera/Motorized_Focus_Camera_Snapshot.py
+++ Motorized_Focus_Camera/Motorized_Focus_Camera_Snapshot.py
@@ -27,7 +27,7 @@ def runFocus():
               dat1 = (value>>8)&0x3f
 	      dat2 = value & 0xf0
               
-              os.system("i2cset -y 1 0x0c %d %d" % (dat1,dat2))
+              os.system("i2cset -y 0 0x0c %d %d" % (dat1,dat2))
 	      camera.capture('Image%d.jpg' %(time.time()))
 	      print "Image save OK"
            elif event.key==K_DOWN:
@@ -39,7 +39,7 @@ def runFocus():
               value = (temp_val<<4) & 0x3ff0
               dat1 = (value>>8)&0x3f
               dat2 = value & 0xf0
-              os.system("i2cset -y 1 0x0c %d %d" % (dat1,dat2))
+              os.system("i2cset -y 0 0x0c %d %d" % (dat1,dat2))
 	      camera.capture('Image%d.jpg' %(time.time()))
               print "Image save OK"
 if __name__ == "__main__":
