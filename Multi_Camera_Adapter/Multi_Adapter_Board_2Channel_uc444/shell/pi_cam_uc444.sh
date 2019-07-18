6344c6518b1a25eb9f81a8d44769369e36a89193
UCTRONICS <avahe.uctronics.com>
2019-07-18 17:01:55 +0800

    Create pi_cam_uc444.sh

---
+++ Multi_Camera_Adapter/Multi_Adapter_Board_2Channel_uc444/shell/pi_cam_uc444.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+i2cset -y 1 0x70 0x00 0x01
+gpio -g mode 17 out
+gpio -g mode 4  out
+gpio -g write 17 0 #set the gpio17 high
+gpio -g write 4 0 #set the gpio4   high
+echo "Choose camera A"
+raspistill -o camera1.jpg
+i2cset -y 1 0x70 0x00 0x02
+gpio -g write 4 1 #set the gpio17 low
+echo "Choose Camera B"
+raspistill -o camera2.jpg
+echo "Test OK"
