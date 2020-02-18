bd1e77fcf5bc46f9f33ec224ba9bcc7512f604c8
arducam <admin@arducam.com>
2020-02-18 17:16:20 +0800

    Update pi_cam_uc444.sh

--- Multi_Camera_Adapter/Multi_Adapter_Board_2Channel_uc444/shell/pi_cam_uc444.sh
+++ Multi_Camera_Adapter/Multi_Adapter_Board_2Channel_uc444/shell/pi_cam_uc444.sh
@@ -7,7 +7,7 @@ gpio -g write 4 0 #set the gpio4   high
 echo "Choose camera A"
 raspistill -o camera1.jpg
 i2cset -y 1 0x70 0x00 0x02
-gpio -g write 4 1 #set the gpio17 low
+gpio -g write 4 1 #set the gpio4 low
 echo "Choose Camera B"
 raspistill -o camera2.jpg
 echo "Test OK"
