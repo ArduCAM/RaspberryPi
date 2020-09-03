bf1665aba72ab7a0f78491d76d5d2ef5330d44ac
Lee <admin@arducam.com>
2020-09-03 08:46:00 +0800

    update init_camera

--- Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_C++/init_camera.sh
+++ Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_C++/init_camera.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 echo "----Test i2c1"
 i2cdetect -y 1
-echo "----Configure Adapter Board V2.2"
 i2cset -y 1 0x70 0x00 0x04
-echo "Adapter Board V2.1 configure OK"
-echo "----install v4l2 driver"
-sudo modprobe bcm2835-v4l2
-echo "v4l2 driver install OK" 
 echo "----Detecting the /dev/video0 device"
 if [ -c "/dev/video0" ]; then
 echo "video0 detected!"
