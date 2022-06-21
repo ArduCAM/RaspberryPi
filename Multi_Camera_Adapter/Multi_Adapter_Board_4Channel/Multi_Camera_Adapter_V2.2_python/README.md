fd492234228ec2278af55ec0d19c15d94585d21e
Lee <admin@arducam.com>
2022-06-21 09:08:15 +0800

    Update previewOpencv.py

---
+++ Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/README.md
@@ -0,0 +1,83 @@
+# Quick start
+
+# Installation
+```Bash
+sudo apt-get install wiringpi
+```
+<!-- ```Bash
+sudo apt-get install libopencv-dev
+``` -->
+```Bash
+sudo apt remove libcamera0
+sudo apt install ./libcamera-dev-0.0.7-bullseye-armhf.deb
+sudo apt install ./libcamera-apps-0.0.7-bullseye-armhf.deb
+wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+chmod +x install_pivariety_pkgs.sh
+./install_pivariety_pkgs.sh -p libcamera_dev
+./install_pivariety_pkgs.sh -p libcamera_apps
+./install_pivariety_pkgs.sh -p imx519_kernel_driver
+```
+```Bash
+sudo apt-get upgrade
+sudo apt install -y python3-kms++
+sudo apt install -y python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg
+sudo pip3 install numpy --upgrade
+sudo pip3 install picamera2
+```
+## Add support for Pi4 B
+- Just a quick post to let you know that you’ll need a new wiringPi for the Raspberry Pi 4B.
+- To upgrade:
+```bash
+cd /tmp
+```
+```bash
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+```
+```bash
+sudo dpkg -i wiringpi-latest.deb
+```
+# test
+
+- Download the RaspberryPi library 
+
+```Bash
+git clone https://github.com/ArduCAM/RaspberryPi.git
+```
+```Bash
+cd RaspberryPi/Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/ 
+```
+```Bash
+sudo chmod +x init_camera.sh
+```
+```Bash
+sudo ./init_camera.sh
+```
+# Make
+```Bash
+cd RaspberryPi/Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/ 
+```
+```Bash
+make 
+```
+
+# Running your RPiCameras with ArduCam
+```Bash
+cd RaspberryPi/Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/ 
+```
+* For the use of remote control terminals, first specify the location of the image display
+```Bash
+export DISPLAY=:0
+```
+```Bash
+ python PiAdapterBoard.py
+```
+![Alt text](https://github.com/ArduCAM/RaspberryPi/blob/master/data/Multi_Camera_Adapter_Board_openCV3.0.png)
+
+
+## Troubleshoot
+- Can't find video0 
+	- check if the i2c bus is normal
+		- add dtparam=i2c_vc=on to /boot/config.txt,then reboot 
+		- run i2cdetect -y 0 to check the devive's address.
+		- if it does not detect the device's address, please check the ribbon cable connection to ensure it is not reversed.
+		
--- Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/previewOpencv.py
+++ Multi_Camera_Adapter/Multi_Adapter_Board_4Channel/Multi_Camera_Adapter_V2.2_python/previewOpencv.py
@@ -1,10 +1,138 @@
-import AdapterBoard 
-import cv2 as cv 
-import numpy as np  
 
-Arducam_adapter_board = AdapterBoard.MultiAdapter()
+from ast import Try
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout, QApplication, QWidget
+from cv2 import goodFeaturesToTrack
+from picamera2 import Picamera2
+from PyQt5.QtGui import QImage,QPixmap
+from PyQt5.QtCore import QThread
+import RPi.GPIO as gp
+import time
+import os
+
+width = 320
+height = 240 
+
+adapter_info = {  
+    "A" : {   
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x04",
+        "gpio_sta":[0,0,1],
+    }, "B" : {
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x05",
+        "gpio_sta":[1,0,1],
+    }, "C" : {
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x06",
+        "gpio_sta":[0,1,0],
+    },"D" : {
+        "i2c_cmd":"i2cset -y 10 0x70 0x00 0x07",
+        "gpio_sta":[1,1,0],
+    }
+}
+
+class WorkThread(QThread):
+
+    def __init__(self):
+        super(WorkThread,self).__init__()
+        gp.setwarnings(False)
+        gp.setmode(gp.BOARD)
+        gp.setup(7, gp.OUT)
+        gp.setup(11, gp.OUT)
+        gp.setup(12, gp.OUT)
+
+
+    def select_channel(self,index):
+        channel_info = adapter_info.get(index)
+        if channel_info == None:
+            print("Can't get this info")
+        gpio_sta = channel_info["gpio_sta"] # gpio write
+        gp.output(7, gpio_sta[0])
+        gp.output(11, gpio_sta[1])
+        gp.output(12, gpio_sta[2])
+
+    def init_i2c(self,index):
+        channel_info = adapter_info.get(index)
+        os.system(channel_info["i2c_cmd"]) # i2c write
+
+    def run(self):
+        global picam2
+        # picam2 = Picamera2()
+        # picam2.configure( picam2.still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=1))
+
+        flag = False
+
+        for item in {"A","B","C","D"}:
+            try:
+                self.select_channel(item)
+                self.init_i2c(item)
+                time.sleep(0.5) 
+                if flag == False:
+                    flag = True
+                else :
+                    picam2.close()
+                    # time.sleep(0.5) 
+                print("init1 "+ item)
+                picam2 = Picamera2()
+                picam2.configure(picam2.still_configuration(main={"size": (320, 240),"format": "BGR888"},buffer_count=2)) 
+                picam2.start()
+                time.sleep(2)
+                picam2.capture_array(wait=False)
+                time.sleep(0.1)
+            except Exception as e:
+                print("except: "+str(e))
+
+        while True:
+            for item in {"A","B","C","D"}:
+                self.select_channel(item)
+                time.sleep(0.02)
+                try:
+                    buf = picam2.capture_array()
+                    buf = picam2.capture_array()
+                    cvimg = QImage(buf, width, height,QImage.Format_RGB888)
+                    pixmap = QPixmap(cvimg)
+                    if item == 'A':
+                        image_label.setPixmap(pixmap)
+                    elif item == 'B':
+                        image_label2.setPixmap(pixmap)
+                    elif item == 'C':
+                        image_label3.setPixmap(pixmap)
+                    elif item == 'D':
+                        image_label4.setPixmap(pixmap)
+                except Exception as e:
+                    print("capture_buffer: "+ str(e))
+
+app = QApplication([])
+window = QWidget()
+layout_h = QHBoxLayout()
+layout_h2 = QHBoxLayout()
+layout_v = QVBoxLayout()
+image_label = QLabel()
+image_label2 = QLabel()
+image_label3 = QLabel()
+image_label4 = QLabel()
+
+# picam2 = Picamera2()
+
+work = WorkThread()
+
 if __name__ == "__main__":
-    Arducam_adapter_board.init(320,240)
-    Arducam_adapter_board.preview()
+    image_label.setFixedSize(320, 240)
+    image_label2.setFixedSize(320, 240)
+    image_label2.setFixedSize(320, 240)
+    image_label2.setFixedSize(320, 240)
+    window.setWindowTitle("Qt Picamera2 Arducam Multi Camera Demo")
+    layout_h.addWidget(image_label)    
+    layout_h.addWidget(image_label2)
+    layout_h2.addWidget(image_label3)
+    layout_h2.addWidget(image_label4)
+    layout_v.addLayout(layout_h,20)
+    layout_v.addLayout(layout_h2,20)
+    window.setLayout(layout_v)
+    window.resize(660, 500)
 
+    work.start()
     
+    window.show()
+    app.exec()
+    work.quit()
+    picam2.close()
+
+
